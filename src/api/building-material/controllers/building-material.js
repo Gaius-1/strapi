17ea638ad03fb594e563a7be475f119c978d7732
Gaius-1 <agbofrederick230@gmail.com>
2025-07-05 21:12:35 +0000

    Add building material schema and controller

---
+++ src/api/building-material/controllers/building-material.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * building-material controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::building-material.building-material');
